077ebe7da9e39a37b1c5b8d439cf2a1b8aa022ef
Dainis Dilevka <thedenisovan@proton.me>
2025-11-26 13:19:57 +0000

    Add: file structure and set up server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-const express = require('express');
-require('dotenv').config();
 import type { Request, Response, NextFunction } from 'express';
+const express = require('express');
+const path = require('node:path');
+require('dotenv').config();
 
 const app = express();
 const SERVER = process.env.SERVER;
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
 
-app.get('/', (req: Request, res: Response) => res.send('Hi son!'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req: Request, res: Response) => res.render('index'));
 
 app.listen(SERVER, () => {
   console.log(`Listening on localhost:${SERVER}`);
