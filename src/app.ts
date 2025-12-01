f0c88c9220390b8936c42be40e50f0d12a9e0e44
Dainis Dilevka <thedenisovan@proton.me>
2025-12-01 15:51:07 +0000

    Edit: seperated server and main app in two files

---
+++ src/app.ts
@@ -0,0 +1,92 @@
+import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import path from 'path';
+import dotenv from 'dotenv';
+import { pool } from './db/pool';
+
+import './controllers/authentication';
+import { signin } from './routes/signin';
+import { signup } from './routes/signup';
+import { mainPage } from './routes/mainPage';
+
+const pgSession = require('connect-pg-simple')(session);
+dotenv.config();
+
+// Interface extensions
+declare module 'express-session' {
+  interface SessionData {
+    views: number;
+  }
+}
+declare module 'express-serve-static-core' {
+  interface Request {
+    myName: string;
+  }
+}
+declare global {
+  namespace Express {
+    interface User {
+      id?: number;
+      name: string;
+      surname: string;
+      email: string;
+      pass: string;
+      isMember: string;
+      isAdmin: string;
+    }
+  }
+}
+
+const app = express();
+const SECRET = process.env.NOT_FOR_YOU as string;
+
+// Paths
+const assetsPath = path.join(__dirname, 'public');
+const viewsPath = path.join(__dirname, 'views');
+
+// Middleware
+app.use(express.static(assetsPath));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 2, // 2 days
+    },
+    store: new pgSession({
+      pool,
+      tableName: 'session',
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// View engine
+app.set('views', viewsPath);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', signin);
+app.use('/signup', signup);
+app.use('/mainPage', mainPage);
+
+app.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+app.get('{*splat}', (_req, res) => {
+  res.status(404);
+  res.render('404');
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,10 @@
-import express from 'express';
-import session from 'express-session';
-import passport from 'passport';
-import path from 'path';
 import dotenv from 'dotenv';
-import { pool } from './db/pool';
+import app from './app';
 
-import './controllers/authentication';
-import { signin } from './routes/signin';
-import { signup } from './routes/signup';
-import { mainPage } from './routes/mainPage';
-
-const pgSession = require('connect-pg-simple')(session);
 dotenv.config();
 
-// Interface extensions so i customize session and req metadata via ts
-declare module 'express-session' {
-  interface SessionData {
-    views: number;
-  }
-}
-declare module 'express-serve-static-core' {
-  interface Request {
-    myName: string;
-  }
-}
+const PORT = process.env.SERVER || 3000;
 
-declare global {
-  namespace Express {
-    interface User {
-      id?: number;
-      name: string;
-      surname: string;
-      email: string;
-      pass: string;
-      isMember: string;
-      isAdmin: string;
-    }
-  }
-}
-
-const app = express();
-const SECRET = process.env.NOT_FOR_YOU as string;
-
-// Paths
-const assetsPath = path.join(__dirname, 'public');
-const viewsPath = path.join(__dirname, 'views');
-
-// Middleware
-app.use(express.static(assetsPath));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    secret: SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 2, // Erases session cookies after 2 days of inactivity
-    },
-    store: new pgSession({
-      pool,
-      tableName: 'session',
-    }),
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-// View engine
-app.set('views', viewsPath);
-app.set('view engine', 'ejs');
-
-// Routes
-app.use('/', signin);
-app.use('/signup', signup);
-app.use('/mainPage', mainPage);
-
-app.get('/log-out', (req, res, next) => {
-  req.logout((err) => {
-    if (err) return next(err);
-    res.redirect('/');
-  });
-});
-
-app.get('{*splat}', (_req, res) => {
-  res.status(404);
-  res.render('404');
-});
-
-// Start server
-app.listen(process.env.SERVER, () => {
-  console.log(`Server listening on http://localhost:${process.env.SERVER}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
