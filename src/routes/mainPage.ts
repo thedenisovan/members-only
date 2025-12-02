6157e1a66667d1a7fdca5ec52d6b9a4204813543
Dainis Dilevka <thedenisovan@proton.me>
2025-12-02 11:15:02 +0000

    Feat: added function to generate random color for each card

--- src/routes/mainPage.ts
+++ src/routes/mainPage.ts
@@ -11,5 +11,25 @@ mainPage.get('/', async (req: Request, res: Response) => {
   res.status(200).render('mainPage', {
     user,
     rows,
+    getRandomColor,
   });
 });
+
+function getRandomColor(): string {
+  const luckyNum = Math.floor(Math.random() * 6);
+
+  switch (luckyNum) {
+    case 0:
+      return '#FEF08A;';
+    case 1:
+      return '#FBCFE8;';
+    case 2:
+      return '#BFDBFE;';
+    case 3:
+      return '#BBF7D0;';
+    case 4:
+      return '#E9D5FF;';
+    default:
+      return '#FED7AA;';
+  }
+}
